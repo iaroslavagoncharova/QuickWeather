6b03f728015770d7446094c85de0c7b21973db92
Iaroslava Goncharova <111697910+iaroslavagoncharova@users.noreply.github.com>
2024-01-05 20:40:15 +0200

    deleted backend

--- src/weather-router.mjs
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getWeatherData } from './weather-controller.mjs';
-
-
-const weatherRouter = express.Router();
-weatherRouter.get('/weather/:time/:parameters/:locations/:format', getWeatherData);
-
-export default weatherRouter;
-
